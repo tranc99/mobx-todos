dc6555c6b12ace80ff38e2c126d2494f2de6017f
Tendai Mutunhire <tmutunhire@gmail.com>
2017-04-13 15:27:41 +0200

    add test for clearing completed todos

--- 6-mobx-react/__tests__/TodoStore.test.js
+++ 6-mobx-react/__tests__/TodoStore.test.js
@@ -9,4 +9,18 @@ describe('TodoStore', () => {
     expect(store.todos.length).toBe(3);
     expect(store.todos[0].value).toBe('todo1');
   });
+
+  it("clears checked todos", () => {
+    const store = new TodoStore;
+    store.createTodo("todotodo");
+    store.createTodo("todo2");
+    store.createTodo("todo3");
+    store.todos[1].complete = true;
+    store.todos[2].complete = true;
+    store.clearComplete();
+
+    // now check store todos length
+    expect(store.todos.length).toBe(1);
+    expect(store.todos[0].value).toBe("todotodo");
+  });
 });

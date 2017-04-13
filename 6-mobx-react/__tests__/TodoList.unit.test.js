2185d3ac8667f3128bc949286682073035d79b15
Tendai Mutunhire <tmutunhire@gmail.com>
2017-04-13 18:06:21 +0200

    problems with enzyme deps

--- 6-mobx-react/__tests__/TodoList.unit.test.js
+++ 6-mobx-react/__tests__/TodoList.unit.test.js
@@ -1,8 +1,30 @@
-import { shallow } from 'enzyme'
+// import { shallow } from 'enzyme'
 import React from "react"
 
 import TodoList from "../src/js/TodoList"
 
 describe("TodoList", function() {
-  
+  beforeEach(function() {
+    this.store = {
+      filteredTodos: [
+        {value: "todo1", id: 111, complete: false},
+        {value: "todo2", id: 222, complete: false},
+        {value: "todo3", id: 333, complete: false},
+      ],
+      filter: "test",
+      createTodo: (val) => {
+        this.createTodoCalled = true;
+        this.todoValue = val
+      },
+    }
+  });
+
+  // tests will go here and receive this.store
+
+  it("renders filtered todos", function() {
+    //const wrapper = shallow(<TodoList store={this.store} />);
+
+    expect(this.store.filteredTodos[0].value).toBe("todo1");
+
+  });
 });
